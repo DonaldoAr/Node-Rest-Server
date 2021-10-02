b79e437aae63f5c9baa8796a5da834464592e9b9
DonaldoAr <jefe.ceo@gmail.com>
2021-10-01 21:30:39 -0500

    Add a check for the information and function for validate fields

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,8 @@ const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/user');
 const user = require('../models/user');
-const { validationResult } = require('express-validator');
+
+
 
 const userGet = (req = request, res = response)=>{
     const {q, nombre='No name', key} = req.query;
@@ -28,10 +29,7 @@ const userPut = (req, res = response)=>{
 }
 
 const userPost = async (req, res = response)=>{
-    const errors = validationResult(req);
-    if( !errors.isEmpty()){
-        return res.status(400).json(errors);
-    }
+    
     
     const {name, email, password, role} = req.body; // Destructuration
     const usuario = new Usuario( {name, email, password, role} ); 
--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const { userGet,
         userPost, 
         userDelete, 
         userPatch } = require('../controllers/users');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
@@ -13,7 +14,11 @@ router.get('/',   userGet);
 router.put('/:id',   userPut);
 // When we define a middleware we sent an array before the controller
 router.post('/',[
+        check('name', 'The name should be exist').not().isEmpty(),
+        check('password','The password should be exist and mush have more than 6 letters').isLength({ min:6 }),
         check('email', 'The email does not valid').isEmail(),
+        check('role', 'Th rol is not valid').isIn(['ADMIN_ROLE','USER_ROLE']),
+        validateFields
 ],  userPost);
 router.delete('/', userDelete);
 router.patch('/', userPatch);
